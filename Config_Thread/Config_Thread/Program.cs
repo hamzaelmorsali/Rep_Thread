f7ef22ebce3f2319a8f2e69a0626861f6cfb4ad2
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-22 09:05:48 +0200

    creazione di una istanza thread

---
+++ Config_Thread/Config_Thread/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Config_Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(() =>
+            {
+                  Console.WriteLine("MyThread start");
+                Thread.Sleep(1000);
+                Console.WriteLine("MyThread finish");
+
+
+            });
+            myThread.Start();
+            Thread.Sleep(500);
+            Console.WriteLine("Main Thread ");
+            Console.ReadLine();
+
+
+        }
+    }
+}
