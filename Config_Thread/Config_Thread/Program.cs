bd410c6be6289fcb54fcf587ad98fb2938eec35c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-22 09:50:37 +0200

    esecuizione dei Thread

--- Config_Thread/Config_Thread/Program.cs
+++ Config_Thread/Config_Thread/Program.cs
@@ -22,8 +22,51 @@ namespace Config_Thread
             myThread.Start();
             Thread.Sleep(500);
             Console.WriteLine("Main Thread ");
-            Console.ReadLine();
+            
 
+            string SomeVariable = "hamza el morsali";
+
+            var workerThread = new Thread((o) =>
+            {
+                Console.WriteLine("Saluti da:{0}", o);
+
+             });
+            workerThread.Start(SomeVariable);
+            var list = new List<Thread>();
+            for (int index = 0; index < 5; index++)
+            {
+                var myThread2 = new Thread((currentindex) =>
+                {
+                    Console.WriteLine("Thread number"+" "+"{0} start", currentindex);
+                    Thread.Sleep(500);
+                                       
+                    Console.WriteLine("Thread number"+" "+"{0} finish", currentindex);
+
+                });
+                myThread2.Start(index);
+                list.Add(myThread2);
+            }
+
+            foreach(Thread thread in list)
+            {
+                thread.Join();
+            }
+            Console.WriteLine("execution af all thread finish");
+            var workerthread=new Thread(()=>
+            {
+                Console.WriteLine("thread more long start");
+                Thread.Sleep(5000);
+                Console.WriteLine(" worker Thread finish");
+
+            });
+            workerthread.Start();
+            workerthread.Join(500);
+            if (workerthread.ThreadState != ThreadState.Stopped)
+            {
+                workerthread.Abort();
+            }
+            Console.WriteLine("Application finish");
+            Console.ReadLine();
 
         }
     }
